fb8140e3cc0f4724e9c8bc1bdc49ffeb41db0860
Kaparotu Usha Kiran Mayi <139610240+Usha66@users.noreply.github.com>
2025-07-31 20:29:16 +0530

    Update ChatbotAPI.js

--- Team 7 Fire Fortifiers 22.1/fire/src/ChatbotAPI.js
+++ Team 7 Fire Fortifiers 22.1/fire/src/ChatbotAPI.js
@@ -1,14 +1,20 @@
-import { searchSimilarQuestions } from "./lang.mjs";
 const API = {
   GetChatbotResponse: async message => {
     try {
-      const botMessage = await searchSimilarQuestions(message);
-      return botMessage;
+      const res = await fetch("http://localhost:5000/api/ask", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ question: message })
+      });
+      const data = await res.json();
+      return data.response;
     } catch (error) {
       console.error("Error fetching Bot response:", error);
       return "Error: Failed to fetch response";
     }
-  },
+  }
 };
 
 export default API;
